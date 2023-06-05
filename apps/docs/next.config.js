3f5e976eb28927051f85d5ebe4aad7937669070d
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-06-05 11:16:41 +0800

    feat: support mdx in docs app

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,3 +1,10 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    mdxRs: true,
+  },
   reactStrictMode: true,
 };
+
+const withMDX = require("@next/mdx")();
+module.exports = withMDX(nextConfig);
