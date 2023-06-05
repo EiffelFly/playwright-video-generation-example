4d0bb4693cf2aa34755dd98e86346366ae1cc668
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-06-05 11:16:51 +0800

    feat: add mock documentation

---
+++ apps/docs/src/app/intro/page.tsx
@@ -0,0 +1,9 @@
+import Content from "./content.mdx";
+
+export default function Page() {
+  return (
+    <article className="prose prose-slate lg:prose-xl font-sans">
+      <Content />
+    </article>
+  );
+}
