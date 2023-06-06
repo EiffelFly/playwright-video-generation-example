58e770e05671b9b4270de11701894b20e668e5f9
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-06-06 11:29:59 +0800

    chore: update Tailwind and prettier setting

--- apps/nextjs-app-example/prettier.config.js
+++ apps/nextjs-app-example/prettier.config.js
@@ -2,7 +2,7 @@ module.exports = {
   arrowParens: 'always',
   semi: true,
   trailingComma: 'all',
-  singleQuote: true,
+  singleQuote: false,
   // pnpm doesn't support plugin autoloading
   // https://github.com/tailwindlabs/prettier-plugin-tailwindcss#installation
   plugins: [require('prettier-plugin-tailwindcss')],
